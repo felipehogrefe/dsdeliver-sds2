8c44fb1d31831f3dde43f4d22b7d1c475d29865a
felipehogrefe <felipehogrefe@gmail.com>
2021-11-30 21:30:49 -0300

    Implements services, repositories and controllers for '/orders' endpoint

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.dto;
 
+import com.devsuperior.dsdeliver.entities.Order;
 import com.devsuperior.dsdeliver.entities.OrderStatus;
 import com.devsuperior.dsdeliver.entities.Product;
 
@@ -8,10 +9,13 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class OrderDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
     private Long id;
     private Double longitude;
     private Double latitude;
@@ -19,17 +23,78 @@ public class OrderDTO implements Serializable {
     private Instant moment;
     private OrderStatus status;
 
-    private Set<Product> products = new HashSet<>();
+    private List<ProductDTO> products = new ArrayList<>();
 
     public OrderDTO() {}
 
-    public OrderDTO(Long id, Double longitude, Double latitude, String address, Instant moment, OrderStatus status, Set<Product> products) {
+    public OrderDTO(Long id, Double longitude, Double latitude, String address, Instant moment, OrderStatus status, Set<ProductDTO> products) {
         this.id = id;
         this.longitude = longitude;
         this.latitude = latitude;
         this.address = address;
         this.moment = moment;
         this.status = status;
-        this.products = products;
+    }
+
+    public OrderDTO(Order order) {
+        id = order.getId();
+        longitude = order.getLongitude();
+        latitude = order.getLatitude();
+        address = order.getAddress();
+        moment = order.getMoment();
+        status = order.getStatus();
+        products = order.getProducts().stream().map(ProductDTO::new).collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public List<ProductDTO> getProducts() {
+        return products;
     }
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
@@ -5,6 +5,7 @@ import com.devsuperior.dsdeliver.entities.Product;
 import java.io.Serializable;
 
 public class ProductDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
     private Long id;
     private String name;
     private Double price;
