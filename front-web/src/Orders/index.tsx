76c70907c1b3fcae17a2821810162959dbd5876f
felipehogrefe <felipehogrefe@gmail.com>
2021-12-02 20:31:00 -0300

    Configures the order location mechanic

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -4,9 +4,10 @@ import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepsHeaders from './StepsHeaders';
 import './styles.css'
-import { Product } from './types';
+import { OrderLocationData, Product } from './types';
 
 function Orders(){
+    const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
@@ -19,7 +20,7 @@ function Orders(){
         <div className="orders-container">
             <StepsHeaders />
             <ProductsList products={products}/>
-            <OrderLocation />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
         </div>
     )
 }
