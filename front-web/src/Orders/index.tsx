a9f78a0dbce883e39a65f45fd2fe8105df06cff5
felipehogrefe <felipehogrefe@gmail.com>
2021-12-02 19:41:51 -0300

    Configures the system to use the react leaflet api, for that I needed to fix some configurations, as seen in https://stackoverflow.com/questions/67551922/cra-react-leaflet-failed-to-compile

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepsHeaders from './StepsHeaders';
 import './styles.css'
@@ -18,6 +19,7 @@ function Orders(){
         <div className="orders-container">
             <StepsHeaders />
             <ProductsList products={products}/>
+            <OrderLocation />
         </div>
     )
 }
--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080'
+const API_URL = 'https://felipe-hogrefe-sds2.herokuapp.com'
 
 export function fetchProducts(){
     return axios(`${API_URL}/products`)
